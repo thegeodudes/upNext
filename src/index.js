1b9cf78bba71d6a15e33d3ee68b868c1b26ec3f1
Yale Yng-Wong <56368472+ywy-w@users.noreply.github.com>
2022-05-23 17:10:43 -0400

    more setup

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+import App from './App';
+import store from './features/store';
+
+// import styles from './styles/app.scss';
+// MUI theme
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#bc42f5',
+    },
+    secondary: {
+      main: '#f0f6fa',
+    },
+  },
+});
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ThemeProvider>
+);
