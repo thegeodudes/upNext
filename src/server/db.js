ace7dca26ac27f492bf1105a77433ca69f6a0ebc
Yale Yng-Wong <56368472+ywy-w@users.noreply.github.com>
2022-05-24 11:11:46 -0400

    db setup

---
+++ src/server/db.js
@@ -0,0 +1,21 @@
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+let PG_URI;
+if (process.env.TEST === 'true') {
+  PG_URI = 'postgres://wnulfkvp:zN9ZAJbMHbvnV_KaTTH84n9F-Ogry24P@fanny.db.elephantsql.com/wnulfkvp';
+} else {
+  PG_URI = 'postgres://vqmiicos:wN4X4TRN4VtVoaYnjNfB0ltAXFQOPjAc@fanny.db.elephantsql.com/vqmiicos';
+}
+
+const pool = new Pool({
+  connectionString: PG_URI,
+  // idleTimeoutMillis: 2000,
+  // allowExitOnIdle: true,
+});
+
+export default {
+  query: (text, params, callback) => pool.query(text, params, callback),
+  end: () => pool.end(),
+};
