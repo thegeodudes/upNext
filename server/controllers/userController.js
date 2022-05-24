a2fef157713ad157313b8e8920f95c75a2cccdd1
Carly Yarnell <carly.yarnell21@gmail.com>
2022-05-24 14:47:22 -0400

    user and show routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,23 +1,23 @@
+const db = require('../db');
+
 const bcrypt = require('bcrypt');
 const SALT_WORK_FACTOR = 10;
-
-//require in sql db
 
 const userController = {};
 
 userController.signup = async (req, res, next) => {
-  const { name, username, password } = req.body;
+  const { username, password } = req.body;
   try {
     console.log('in create user');
-    console.log(name, username, password)
-    const hash = await bcrypt.hash(password, 10);
-    const params = [name, username, hash];
-    const queryString = 'INSERT INTO users(name, username, password) VALUES($1, $2, $3) RETURNING id;';
+    console.log(username, password);
+    const hash = await bcrypt.hash(password, SALT_WORK_FACTOR);
+    const params = [username, hash];
+    const queryString = 'INSERT INTO users(username, password) VALUES($1, $2) RETURNING id;';
     const result = await db.query(queryString, params);
-    console.log('after query')
+    console.log('after query');
     res.locals.result = result.rowCount;
     res.locals.userId = result.rows[0].id;
-    console.log('after setting')
+    console.log('after setting');
   } catch (err) {
     console.log('error:', err);
     return next(err);
@@ -37,3 +37,5 @@ userController.login = async (req, res, next) => {
   }
   return next();
 };
+
+module.exports = userController;
